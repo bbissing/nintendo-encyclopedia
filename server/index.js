3881efe1f0046a6ac298771b75b77df9523b2bb0
bbissing <bbissing101@gmail.com>
2023-03-26 13:54:35 -0500

    basic client and server setup. ran express to serve up a basic html static file with some basic styling

---
+++ server/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, '../client')));
+app.use(express.urlencoded({extended: true}));
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(`Listening on http://localhost:${PORT}`);
+  console.log('path: ', path.join(__dirname, '../client'));
+});
