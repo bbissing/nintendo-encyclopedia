15d16e3208afd5d2592dc28a599ad1b45971688e
bbissing <bbissing101@gmail.com>
2023-04-03 21:40:00 -0500

    installed react-router. each card on the homepage now links to their own respective page.

---
+++ client/src/components/SearchGames.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+function SearchGames() {
+
+  return (
+    <>
+      <h4>Search Games</h4>
+    </>
+  )
+}
+
+export default SearchGames;
---
+++ client/src/components/WriteReview.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+function WriteReview() {
+
+  return (
+    <>
+      <h4>Write Review</h4>
+    </>
+  )
+}
+
+export default WriteReview;
