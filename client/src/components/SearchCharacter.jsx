db4288b663aa7a9b624607d60b98ba4dffca3478
bbissing <bbissing101@gmail.com>
2023-04-08 12:48:55 -0500

    added basic search bar to 'Search Characters' page

--- client/src/components/SearchCharacter.jsx
+++ client/src/components/SearchCharacter.jsx
@@ -1,11 +1,22 @@
 import React, { useState, useEffect } from 'react';
+import { FiSearch } from 'react-icons/fi';
+import { IconContext } from "react-icons";
+
+
 
 function SearchCharacter() {
 
   return (
-    <>
-      <h4>Search Character</h4>
-    </>
+    <div className="wrap">
+      <div className="search">
+        <input type="text" className="searchTerm" placeholder="Search any character"/>
+        <button type="submit" className="searchButton">
+          <IconContext.Provider value={{ className: "magnify-glass" }}>
+              <FiSearch />
+          </IconContext.Provider>
+        </button>
+      </div>
+    </div>
   )
 }
 
--- server/index.js
+++ server/index.js
@@ -7,6 +7,18 @@ const PORT = 3000;
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(express.urlencoded({extended: true}));
 
+// app.get('/search_character')
+
+// ---- Catch all for routing ---- //
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+});
+
 app.listen(PORT, (err) => {
   if (err) {
     console.log(err);
