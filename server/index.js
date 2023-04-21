6b0c3e3cabb0d4606534cbbac9bb37c2082c35c7
bbissing <bbissing101@gmail.com>
2023-04-21 00:20:32 -0500

    initial setup for SearchGames

--- server/index.js
+++ server/index.js
@@ -11,10 +11,20 @@ app.use(express.urlencoded({extended: true}));
 
 
 app.get('/get-character', async (req, res) => {
-  // const response = await axios.get(`https://www.giantbomb.com/api/accessories/?api_key=${process.env.GIANT_BOMB_API_KEY}&format=json&field_list=genres,name`);
-  // console.log('req:', req.query.query);
   const response = await axios.get(`https://www.giantbomb.com/api/character/${req.query.query}/?api_key=${process.env.GIANT_BOMB_API_KEY}&format=json&field_list=name,image,deck`);
   res.send(response.data.results);
+});
+
+app.get('/get-games', async (req, res) => {
+  const allGames = [];
+  let games = await axios.get(`https://www.giantbomb.com/api/character/${req.query.query}/?api_key=${process.env.GIANT_BOMB_API_KEY}&format=json&field_list=games`);
+  games = games.data.results.games;
+  allGames.push(games);
+  // const games = response.data.results.games;
+  // games.forEach(game => {
+
+  // })
+  res.send(allGames);
 });
 
 // ---- Catch all for routing ---- //
