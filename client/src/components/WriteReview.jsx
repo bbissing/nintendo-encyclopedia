3af4a0352858bba5add0319b8f51d742a4909c65
bbissing <bbissing101@gmail.com>
2023-04-24 00:11:45 -0500

    add basic submission form. form contains multiple text inputs and a submit button. functionality still needs to be added

--- client/src/components/WriteReview.jsx
+++ client/src/components/WriteReview.jsx
@@ -1,12 +1,107 @@
 import React, { useState, useEffect } from 'react';
+import { IoArrowBackOutline } from 'react-icons/io5';
+import { Link } from 'react-router-dom';
 
 function WriteReview() {
+  const [modalOpen, setModal] = useState(true);
+
+  function handleSubmit(e) {
+		e.preventDefault();
+    setModal(false);
+	}
 
   return (
     <>
-      <h4>Write Review</h4>
+      <Link to="/">
+        <IoArrowBackOutline className="back-button" color={'#262929'} size={30} />
+      </Link>
+      {modalOpen ? (
+        <div className="ModalForm">
+          <div className="Modal">
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label htmlFor="name"></label>
+                <input id="name" autoComplete="false" required type="text" placeholder="Mario"/>
+              </div>
+              <div>
+                <label htmlFor="review"></label>
+                <input id="review" autoComplete="false" required type="text" placeholder="Write Your Review" maxLength="300"/>
+              </div>
+              <div>
+                <button>Submit</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
     </>
   )
 }
 
 export default WriteReview;
+
+// var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
+
+// var Input = React.createClass({
+// 	render: function() {
+// 		return (
+// 			<div className="Input">
+// 				<label for={this.props.name}></label>
+// 				<input id={this.props.name} autocomplete="false" required type={this.props.type} placeholder={this.props.placeholder} maxLength={this.props.maxlength}/>
+// 			</div>
+// 		);
+// 	}
+// });
+
+// var Modal = React.createClass({
+// 	render: function() {
+// 		return (
+// 			<div className="Modal">
+// 				<form onSubmit={this.props.onSubmit} className="ModalForm">
+// 					<Input name="name" type="text" placeholder="Mario" />
+// 					<Input name="review" type="text" placeholder="Write Your Review" maxlength="300"/>
+// 					<button>Submit <i className="fa fa-fw fa-chevron-right"></i></button>
+// 				</form>
+// 			</div>
+// 		);
+// 	}
+// });
+
+// var App = React.createClass({
+
+// 	getInitialState: function() {
+// 		return { mounted: false };
+// 	},
+
+// 	componentDidMount: function() {
+// 		this.setState({ mounted: true });
+// 	},
+
+// 	handleSubmit: function(e) {
+// 		this.setState({ mounted: false });
+// 		e.preventDefault();
+// 	},
+
+// 	render: function() {
+
+// 		var child;
+// 		if(this.state.mounted) {
+// 			child = (<Modal onSubmit={this.handleSubmit} />);
+// 		}
+
+// 		return(
+// 			<div className="App">
+// 				<ReactCSSTransitionGroup transitionName="example" transitionEnterTimeout={500} transitionLeaveTimeout={300}>
+// 					{child}
+// 				</ReactCSSTransitionGroup>
+// 			</div>
+// 		);
+// 	}
+// });
+
+// ReactDOM.render(
+// 	<App />,
+// 	document.getElementById('app')
+// );
